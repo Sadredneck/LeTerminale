01c53eb961fe1fdf976a646a9fa24b876726d088
Roman <Rolamanbuss@yandex.ru>
2018-02-16 16:30:19 +0300

    roman

---
+++ src/terminale/AllFiles.java
@@ -0,0 +1,17 @@
+package terminale;
+
+public class AllFiles implements Commandable {
+    private String[] param;
+
+    public AllFiles() {
+    }
+
+    public AllFiles(String... param) {
+        this.param = param;
+    }
+
+    @Override
+    public void doCommand() {
+        
+    }
+}
