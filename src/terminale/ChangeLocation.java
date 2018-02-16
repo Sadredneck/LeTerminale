d4799fdcc6b3956ab7fc90d880709bcd7d6b98ca
Roman <Rolamanbuss@yandex.ru>
2018-02-16 17:07:58 +0300

    roman

---
+++ src/terminale/ChangeLocation.java
@@ -0,0 +1,13 @@
+package terminale;
+
+public class ChangeLocation implements Commandable {
+    private String toLocation;
+
+    public ChangeLocation() {
+    }
+
+    @Override
+    public void doCommand() {
+
+    }
+}
