b34957ac2b36011c258cc39e78c345352adcda59
Sergey <gray4747@mail.ru>
2018-02-16 17:12:19 +0300

    continue

---
+++ src/terminale/Commands.java
@@ -0,0 +1,14 @@
+package terminale;
+
+public enum Commands {
+    ALLFILES {
+        public void doWar() {
+            System.out.println("Babah");
+        }
+    },
+    BBBB {};
+
+    public void doWar() {
+        System.out.println("allah");
+    }
+}
--- src/terminale/Term.java
+++ src/terminale/Term.java
@@ -1,7 +1,20 @@
 package terminale;
 
-public class Term {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Term {
+
+    public static void main(String[] args) {
+        BufferedReader inputLine = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Weclome to Terminalus Motherfucker!");
+        while (true) {
+            try {
+                inputLine.readLine();
+            } catch (IOException e) {
+
+            }
+        }
     }
 }
