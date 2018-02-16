095ea24118f79949549e7ae3e4d4928326da32e2
Sergey <gray4747@mail.ru>
2018-02-16 18:14:22 +0300

    term update

--- src/terminale/Term.java
+++ src/terminale/Term.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static terminale.Commands.ALLFILES;
-import static terminale.Commands.COPY;
-
 public class Term {
-
     private static void handleInput(String input) throws IllegalArgumentException {
         String[] elems = input.split("\\s+");
-        System.out.println(elems.length);
-        switch (Commands.valueOf(elems[0].toUpperCase())){
+
+        if (elems.length == 0)
+            return;
+
+        switch (Commands.valueOf(elems[0].toUpperCase())) {
             case ALLFILES:
             case COPY:
             case EXIT:
+                throw new Error();
             case HELP:
             case RENAME:
             case GETLOCATION:
@@ -25,17 +25,19 @@ public class Term {
 
     public static void main(String[] args) {
         BufferedReader inputLine = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Welcome to Terminalus Motherfucker!");
+        System.out.println("Welcome to Terminalus!");
         while (true) {
+            System.out.print("Enter your command: ");
             String line = "";
             try {
                 line = inputLine.readLine();
                 handleInput(line);
             } catch (IOException e) {
                 System.out.println("Bizzare input.");
-            }
-            catch (IllegalArgumentException exc){
+            } catch (IllegalArgumentException exc) {
                 System.out.println("wrong command");
+            } catch (Error error) {
+                System.exit(0);
             }
         }
     }
