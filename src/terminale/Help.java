8f5b281fb85214dfdee3f5d1c3bc6ed96d5f3cd8
Sergey <gray4747@mail.ru>
2018-02-16 19:24:01 +0300

    term update

---
+++ src/terminale/Help.java
@@ -0,0 +1,26 @@
+package terminale;
+
+public class Help implements Commandable{
+
+    private String help = "Command: {ALLFILES} + Parameters: {{-name}, {-time}, none}\n" +
+            "Command: {CHANGELOCATION} + Parameters: {absolute or relative path}\n" +
+            "Command: {GETLOCATION} + Parameters: none\n" +
+            "Command: {COPY} + Parameters: {{absolute or relative path}, {absolute or relative path}}\n" +
+            "Command: {RENAME} + Parameters: {absolute or relative path}\n" +
+            "Command: {HELP} + Parameters: none\n" +
+            "Command: {MAKEZIP} + Parameters: {{absolute or relative path}, {absolute or relative path}}\n" +
+            "Command: {EXIT} + Parameters: none";
+
+    public Help(){
+
+    }
+
+    public Help (String... args){
+
+    }
+
+    @Override
+    public void doCommand() {
+        System.out.println(help);
+    }
+}
