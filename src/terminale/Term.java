960825a2d65f465f95b232710bd80b3c1dc92964
Sergey <gray4747@mail.ru>
2018-02-16 16:36:02 +0300

    Start

--- src/terminale/Term.java
+++ src/terminale/Term.java
@@ -1,9 +1,5 @@
 package terminale;
 
-import java.awt.datatransfer.StringSelection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Term {
     public static void main(String[] args) {
 
