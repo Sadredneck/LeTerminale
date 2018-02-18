acc899645e903a09fc1f7cb6ee817a206fb2069e
Sergey <gray4747@mail.ru>
2018-02-18 15:41:32 +0300

    term update

--- src/terminale/MakeZIP.java
+++ src/terminale/MakeZIP.java
@@ -1,6 +1,7 @@
 package terminale;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
---
+++ src/terminale/Rename.java
@@ -0,0 +1,11 @@
+package terminale;
+
+/**
+ * Created by 1 on 18.02.2018.
+ */
+public class Rename implements Commandable{
+    @Override
+    public void doCommand() {
+
+    }
+}
